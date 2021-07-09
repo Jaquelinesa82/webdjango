f36a7a75a974d5a34118c6289a1a490e1ab4eb6e
jaqueline <jaquelinesa.82@gmail.com>
2021-07-09 01:43:45 -0300

    Criado Bloco de Templates

--- devpro/base/tests/test_home.py
+++ devpro/base/tests/test_home.py
@@ -14,7 +14,7 @@ def test_status_code(resp):
 
 
 def test_title(resp):
-    assert_contains(resp, '<title>DevPro</title>')
+    assert_contains(resp, '<title>DevPro - Home</title>')
 
 
 def test_home_link(resp):
