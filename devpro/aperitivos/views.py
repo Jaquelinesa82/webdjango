d1401d4a37b6b28d8792890d1c0c1639d35a3f9b
jaqueline <jaquelinesa.82@gmail.com>
2021-07-15 22:37:48 -0300

    Lista de Elementos com For

--- devpro/aperitivos/views.py
+++ devpro/aperitivos/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 
+videos = [
+    {'slug': 'motivacao', 'titulo': 'Motivação', 'youtube_id': '9iZLDnW_vwU'},
+    {'slug': 'instalacao', 'titulo': 'Instalação', 'youtube_id': 'XdFUpFUDt88'},
+]
+
+videos_dct = {dct['slug']: dct for dct in videos}
+
+
+def indice(request):
+    return render(request, 'aperitivos/indice.html', context={'videos': videos})
+
 
 def video(request, slug):
-    video = {'titulo': 'Video Aperitivo:' ' Motivação', 'youtube_id': 123215}
+    video = videos_dct[slug]
     return render(request, 'aperitivos/video.html', context={'video': video})
