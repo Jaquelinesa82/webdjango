44b59a36a8303c407538e985d68c8548b5a9a7ef
jaqueline <jaquelinesa.82@gmail.com>
2021-07-20 10:59:21 -0300

    Criação Automágica de Propriedades de Modelo

--- devpro/aperitivos/tests/test_video.py
+++ devpro/aperitivos/tests/test_video.py
@@ -1,14 +1,13 @@
 import pytest
 from django.urls import reverse
+from model_bakery import baker
 from devpro.aperitivos.models import Video
 from devpro.django_assertions import assert_contains
 
 
 @pytest.fixture
 def video(db):
-    v = Video(slug='moticacao', titulo='Video Aperitivo: Motivação', youtube_id='9iZLDnW_vwU')
-    v.save()
-    return v
+    return baker.make(Video)
 
 
 @pytest.fixture
@@ -18,7 +17,7 @@ def resp(client, video):
 
 @pytest.fixture
 def resp_video_nao_encontrado(client, video):
-    return client.get(reverse('aperitivos:video', args=(video.slug + 'video_nao_existente',)))
+    return client.get(reverse('aperitivos:video', args=(video.slug+'video_nao_existente',)))
 
 
 def test_status_code_video_nao_encontrado(resp_video_nao_encontrado):
@@ -33,5 +32,5 @@ def test_titulo_video(resp, video):
     assert_contains(resp, video.titulo)
 
 
-def test_conteudo_video(resp):
+def test_conteudo_video(resp, video):
     assert_contains(resp, f'<iframe src="https://www.youtube.com/embed/{video.youtube_id}"')
