39616d49c1b56dc50ef252247446e079bb78a5a5
jaqueline <jaquelinesa.82@gmail.com>
2021-07-20 23:33:55 -0300

    Processador de Contexto

--- devpro/base/views.py
+++ devpro/base/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from devpro.modulos import facade
+
 
 def home(request):
-    return render(request, 'base/home.html')
+    return render(request, 'base/home.html', {})
