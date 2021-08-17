5957e18333fd48eaad323fb85d892cb86ed16dae
jaqueline <jaquelinesa.82@gmail.com>
2021-08-17 17:23:06 -0300

    Acesso ao lado N do relacionamento

--- devpro/modulos/views.py
+++ devpro/modulos/views.py
@@ -3,14 +3,15 @@ from django.shortcuts import render
 from devpro.modulos import facade
 
 
+def indice(requests):
+    ctx = {'modulos': facade.listar_modulos_com_aulas()}
+    return render(requests, 'modulos/indice.html', ctx)
+
+
 def detalhe(request, slug):
     modulo = facade.encontrar_modulos(slug)
     aulas = facade.listar_aulas_de_modulos_ordenados(modulo)
     return render(request, 'modulos/modulo_detalhe.html', {'modulo': modulo, 'aulas': aulas})
-
-
-def indice(requests):
-    return render(requests, 'modulos/indice.html')
 
 
 def aula(request, slug):
