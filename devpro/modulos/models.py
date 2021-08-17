5740e0265191d24497fafd80af4eddce2bbbb686
jaqueline <jaquelinesa.82@gmail.com>
2021-08-17 12:57:26 -0300

    Como Criar Relacionamento entre Módulo e Aulas

--- devpro/modulos/models.py
+++ devpro/modulos/models.py
@@ -17,3 +17,19 @@ class Modulo(OrderedModel):
 
     def get_absolute_url(self):
         return reverse('modulos:detalhe', kwargs={'slug': self.slug})
+
+
+class Aula(OrderedModel):
+    titulo = models.CharField(max_length=64)
+    slug = models.SlugField(unique=True)
+    modulo = models.ForeignKey('modulo', on_delete=models.Prefetch)
+    order_with_respect_to = 'modulo'
+
+    class Meta(OrderedModel.Meta):
+        pass
+
+    def __str__(self):
+        return self.titulo
+
+    def get_absolute_url(self):
+        return reverse('modulos:aula', kwargs={'slug': self.slug})
