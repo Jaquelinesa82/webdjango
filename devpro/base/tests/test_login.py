7287d0ad7529231bcc4743d5083b4b03edf1e1aa
jaqueline <jaquelinesa.82@gmail.com>
2021-08-18 01:12:52 -0300

    Implementação de Logout

--- devpro/base/tests/test_login.py
+++ devpro/base/tests/test_login.py
@@ -67,3 +67,6 @@ def test_botao_sair_disponivel(resp_home_com_usuario_logado):
 def test_nome_usuario_logado_disponivel(resp_home_com_usuario_logado, usuario_logado):
     assert_not_contains(resp_home_com_usuario_logado, usuario_logado.first_name)
 
+
+def test_botao_de_logout_disponivel(resp_home_com_usuario_logado):
+    assert_not_contains(resp_home_com_usuario_logado, reverse('logout'))
